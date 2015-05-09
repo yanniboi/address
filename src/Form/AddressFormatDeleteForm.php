830cf1c59417c4b56933599c9bc463f767bfc79c
Bojan Zivanovic <bojanz@gmail.com>
2015-05-08 18:36:45 -0700

    Simplify the AddressFormatDeleteForm.

--- src/Form/AddressFormatDeleteForm.php
+++ src/Form/AddressFormatDeleteForm.php
@@ -17,39 +17,12 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class AddressFormatDeleteForm extends EntityDeleteForm {
 
   /**
-   * The country manager.
-   *
-   * @var \Drupal\Core\Locale\CountryManagerInterface
-   */
-  protected $countryManager;
-
-  /**
-   * Creates an AddressFormatDeleteForm instance.
-   *
-   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
-   *   The address format storage.
-   */
-  public function __construct(CountryManagerInterface $countryManager) {
-    $this->countryManager = $countryManager;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static($container->get('countryManager'));
-  }
-
-  /**
    * {@inheritdoc}
    */
   public function getQuestion() {
-    $countries = $this->countryManager->getList();
-    $addressFormat = $this->getEntity();
-
-    return $this->t('Are you sure you want to delete the address format for %country?', array(
-      '%country' => $countries[$addressFormat->getCountryCode()],
-    ));
+    return $this->t('Are you sure you want to delete the address format for %country?', [
+      '%country' => $this->getEntity()->label(),
+    ]);
   }
 
 }
