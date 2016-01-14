9482753c359fe1c7ed0f5ce36159cb6b15e2377b
Xano <Xano@62965.no-reply.drupal.org>
2016-01-14 11:20:55 +0100

    Issue #2626746 by Xano: Expose the zone plugin type

---
+++ src/PluginDefinition/ZoneMemberPluginDefinitionDecorator.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\address\PluginDefinition\ZoneMemberPluginDefinitionDecorator.
+ */
+
+namespace Drupal\address\PluginDefinition;
+
+use Drupal\plugin\PluginDefinition\ArrayPluginDefinitionDecorator;
+
+/**
+ * Provides a zone member plugin definition decorator.
+ */
+class ZoneMemberPluginDefinitionDecorator extends ArrayPluginDefinitionDecorator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setLabel($label) {
+    $this->arrayDefinition['name'] = $label;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLabel() {
+    return isset($this->arrayDefinition['name']) ? $this->arrayDefinition['name'] : NULL;
+  }
+
+}
