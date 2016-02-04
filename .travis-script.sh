90d05a9350445ceeff22762dcdf7002f214925ab
Yan Loetzer <yan@yanniboi.com>
2016-02-04 13:57:13 +0000

    Create .travis-script.sh

---
+++ .travis-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e $DRUPAL_TI_DEBUG
+
+# Run PHPUnit tests and submit code coverage statistics.
+drupal_ti_ensure_drupal
+drupal_ti_ensure_module_linked
+
+cd $DRUPAL_TI_DRUPAL_DIR/core
+$DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit --group address
